e84b19b6adc22803c52a43715eddbb872e3f5659
John Greeley <johnny.greeley@gmail.com>
2015-04-13 23:22:11 -0400

    adding date filtering to specvis

--- dashboard/js/specvis.js
+++ dashboard/js/specvis.js
@@ -17,7 +17,14 @@ SpecVis = function(_parentElement, _data, _eventHandler, _options) {
 SpecVis.prototype.initVis = function() {
     var that = this;
 
+    this.dateFormatter = d3.time.format("%Y-%m-%d");
+    this.radius = Math.min(this.width, this.height) / 2;
+
     this.color = d3.scale.category20c();
+    this.x = d3.scale.linear()
+        .range([0, 2 * Math.PI]);
+    this.y = d3.scale.sqrt()
+        .range([0, this.radius]);
 
     // constructs SVG layout
     this.svg = this.parentElement.append("svg")
@@ -26,12 +33,12 @@ SpecVis.prototype.initVis = function() {
         .append("g")
         .attr("transform", "translate(" + (this.margin.left + this.margin.right + this.width)/2 + "," +(this.margin.top + this.margin.bottom + this.height)/2 + ")");
 
-    this.radius = Math.min(this.width, this.height) / 2;
-
+    //sets up the partition layout
     this.partition = d3.layout.partition()
         .size([2 * Math.PI, this.radius * this.radius])
         .value(function(d) { return 1; });
 
+    //sets up the arc generator for the radial sunburst
     this.arc = d3.svg.arc()
         .startAngle(function(d) { return d.x; })
         .endAngle(function(d) { return d.x + d.dx; })
@@ -50,31 +57,48 @@ SpecVis.prototype.initVis = function() {
 SpecVis.prototype.wrangleData = function(_dateFilterFunction) {
     var that = this;
 
+    //setup the default date filter function (right now filtering from march 1, 2015 to now)
+    var dateFilterFunction = function(d){return new Date(d.created_at) >= new Date("2015-03-01") && new Date(d.created_at) <= new Date()};
+    if (_dateFilterFunction != null){
+        dateFilterFunction = _dateFilterFunction;
+    }
+
+
+    var totalIssues1 = 0;
     //create a lookup table of specs keyed by url
     this.displayData.spec_lookup = {};
-    this.data.specs.forEach(function(d,i) {
-        if(that.data.specs[i]){
-            that.displayData.spec_lookup[that.data.specs[i].url] = that.data.specs[i];
-        } else {
-            console.log(that.data.specs[i]);
+    this.data.specs.forEach(function(d) {
+        //new spec objects need to be created to not filter out items from allData
+        spec = {};
+        spec.url = d.url;
+        spec.name = d.title;
+        if(d.issues) {
+            //console.log(d.issues.filter(dateFilterFunction));
+            spec.issues = d.issues.filter(dateFilterFunction);
+            totalIssues1 += spec.issues.length;
         }
+        that.displayData.spec_lookup[d.url] = spec;
     });
- //   console.log(this.displayData.spec_lookup);
+    console.log(this.displayData.spec_lookup);
+    console.log(totalIssues1);
 
     this.displayData.test_lookup = {};
     this.data.tests.forEach(function(test) {
-        test.specs.forEach(function(spec) {
-            //check if an entry already exists, if not create one
-            if(!that.displayData.test_lookup[spec]) {
-                that.displayData.test_lookup[spec] = [];
-            }
-            that.displayData.test_lookup[spec].push(test);
-        })
+        if(dateFilterFunction(test)) {
+            test.specs.forEach(function (spec) {
+                //check if an entry already exists, if not create one
+                if (!that.displayData.test_lookup[spec]) {
+                    that.displayData.test_lookup[spec] = [];
+                }
+                that.displayData.test_lookup[spec].push(test);
+            })
+        }
     });
- //   console.log(this.displayData.test_lookup);
+//   console.log(this.displayData.test_lookup);
 
     this.displayData.root = {name:"W3C", children:[]};
 
+    var totalIssues2 = 0;
     this.data.groups.forEach(function(d) {
         var group = {};
         group.name = d.name;
@@ -83,41 +107,95 @@ SpecVis.prototype.wrangleData = function(_dateFilterFunction) {
         group.children = [];
         d.specs.forEach(function(dd) {
             var spec = {};
-            spec.name = dd.title;
             spec.url = dd.url;
             spec.children = [{name:"HTML", children:[]},{name:"Tests", children:[]}];
             if(that.displayData.spec_lookup[dd.url]) {
+                spec.name = that.displayData.spec_lookup[dd.url].name;
                 spec.children[0].children = that.displayData.spec_lookup[dd.url].issues;
+                if(spec.children[0].children) {
+                    totalIssues2 += spec.children[0].children.length
+                }
             } else {
                 console.log("Spec Not Found Error: " + dd.url);
             }
             if(that.displayData.test_lookup[dd.url]) {
                 spec.children[1].children = that.displayData.test_lookup[dd.url];
             } else {
-                console.log("No Tests for: " + dd.url);
+               //console.log("No Tests for: " + dd.url);
             }
             group.children.push(spec);
         });
         that.displayData.root.children.push(group);
     });
+    console.log(totalIssues2);
 
-
+    var totalIssues = 0;
+    this.displayData.root.children.forEach(function(d) {
+        d.children.forEach(function(dd) {
+            dd.children.forEach(function(ddd) {
+                if(ddd.children) {
+                   // console.log(ddd.children);
+                    totalIssues+=ddd.children.length;
+                }
+            });
+        });
+    });
+    console.log(totalIssues);
     console.log(this.displayData.root);
-
 };
 
 SpecVis.prototype.updateVis = function() {
     var that = this;
 
+    var click = function(d) {
+        console.log(d);
+    };
+
+    // Setup for switching data: stash the old values for transition.
+    var stash = function(d) {
+        d.x0 = d.x;
+        d.dx0 = d.dx;
+    };
+
+    // When switching data: interpolate the arcs in data space.
+    var arcTweenData = function(a, i) {
+        var oi = d3.interpolate({x: a.x0, dx: a.dx0}, a);
+        function tween(t) {
+            var b = oi(t);
+            a.x0 = b.x;
+            a.dx0 = b.dx;
+            return arc(b);
+        }
+        if (i == 0) {
+            // If we are on the first arc, adjust the x domain to match the root node
+            // at the current zoom level. (We only need to do this once.)
+            var xd = d3.interpolate(that.x.domain(), [node.x, node.x + node.dx]);
+            return function(t) {
+                that.x.domain(xd(t));
+                return tween(t);
+            };
+        } else {
+            return tween;
+        }
+    };
+
+    this.partition(this.displayData.root);
 
     var path = this.svg.datum(this.displayData.root).selectAll("path")
-        .data(this.partition.nodes)
+        .data(this.partition.nodes);
+    path
         .enter().append("path")
         .attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
         .attr("d", this.arc)
         .style("stroke", "#fff")
         .style("fill", function(d) {return that.color((d.children ? d : d.parent).name); })
-        .style("fill-rule", "evenodd");
+        .style("fill-rule", "evenodd")
+        .on("click", click)
+        .each(stash);
+
+//    path.attrTween("d", arcTweenData);
+//    path
+//        .exit().remove();
 
     //var text = this.svg.selectAll("text").data(this.partition.nodes);
     //var textEnter = text.enter().append("text")
@@ -141,42 +219,10 @@ SpecVis.prototype.updateVis = function() {
     //    .attr("x", 0)
     //    .attr("dy", "1em")
     //    .text(function(d) { return d.depth ? d.name.split(" ")[1] || "" : ""; });
-
-
 };
 
 SpecVis.prototype.onSelectionChange = function(selectionStart, selectionEnd) {
-
-
-    this.wrangleData(function(d) {return d.created_at >= selectionStart && d.created_at <=selectionEnd});
+    console.log("select from " + selectionStart + " to " + selectionEnd);
+    this.wrangleData(function(d) {return new Date(d.created_at) >= selectionStart && new Date(d.created_at) <=selectionEnd});
     this.updateVis();
 };
-
-/**
- * The aggregate function that creates the counts for each age for a given filter.
- * @param _filter - A filter can be, e.g.,  a function that is only true for data of a given time range
- * @returns {Array|*}
- */
-SpecVis.prototype.filterAndAggregate = function(_filter){
-    var that = this;
-
-    // Set filter to a function that accepts all items
-    // ONLY if the parameter _filter is NOT null use this parameter
-    var filter = function(){return true;};
-    if (_filter != null){
-        filter = _filter;
-    }
-    //Dear JS hipster, a more hip variant of this construct would be:
-    // var filter = _filter || function(){return true;}
-
-    var filteredSpecIssues = this.data.specs.forEach(function(spec) {
-        if(spec.issues){
-            spec.issues = spec.issues.filter(filter);
-        }
-    });
-
-    return filteredSpecIssues;
-};
-
-
-
