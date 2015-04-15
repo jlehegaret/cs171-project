f67bf5bf57460acc4c705d4676d7e70aab8f4a74
John Greeley <johnny.greeley@gmail.com>
2015-04-14 20:00:24 -0400

    zoom functionality (only on a preselected data set)

--- dashboard/js/specvis.js
+++ dashboard/js/specvis.js
@@ -2,8 +2,8 @@ SpecVis = function(_parentElement, _data, _eventHandler, _options) {
     this.parentElement = _parentElement;
     this.data = _data;
     this.eventHandler = _eventHandler;
-    this.options = _options || {width:700, height:700};
-    this.displayData = [];
+    this.options = _options || {width:800, height:800};
+    this.displayData = {};
     this.allIssues = [];
 
     // defines constants
@@ -35,15 +35,15 @@ SpecVis.prototype.initVis = function() {
 
     //sets up the partition layout
     this.partition = d3.layout.partition()
-        .size([2 * Math.PI, this.radius * this.radius])
+//        .size([2 * Math.PI, this.radius * this.radius])
         .value(function(d) { return 1; });
 
     //sets up the arc generator for the radial sunburst
     this.arc = d3.svg.arc()
-        .startAngle(function(d) { return d.x; })
-        .endAngle(function(d) { return d.x + d.dx; })
-        .innerRadius(function(d) { return Math.sqrt(d.y); })
-        .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
+        .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, that.x(d.x))); })
+        .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, that.x(d.x + d.dx))); })
+        .innerRadius(function(d) { return Math.max(0, that.y(d.y)); })
+        .outerRadius(function(d) { return Math.max(0, that.y(d.y + d.dy)); });
 
 
     // filter, aggregate, modify data
@@ -65,7 +65,7 @@ SpecVis.prototype.wrangleData = function(_dateFilterFunction) {
 
 
     var totalIssues1 = 0;
-    //create a lookup table of specs keyed by url
+    //create a lookup table of specs keyed by spec url
     this.displayData.spec_lookup = {};
     this.data.specs.forEach(function(d) {
         //new spec objects need to be created to not filter out items from allData
@@ -79,9 +79,10 @@ SpecVis.prototype.wrangleData = function(_dateFilterFunction) {
         }
         that.displayData.spec_lookup[d.url] = spec;
     });
-    console.log(this.displayData.spec_lookup);
-    console.log(totalIssues1);
+    //console.log(this.displayData.spec_lookup);
+    //console.log(totalIssues1);
 
+    //creates a lookup table of tests keyed by spec url
     this.displayData.test_lookup = {};
     this.data.tests.forEach(function(test) {
         if(dateFilterFunction(test)) {
@@ -94,62 +95,70 @@ SpecVis.prototype.wrangleData = function(_dateFilterFunction) {
             })
         }
     });
-//   console.log(this.displayData.test_lookup);
+   //console.log(this.displayData.test_lookup);
 
-    this.displayData.root = {name:"W3C", children:[]};
 
+    // Create the root object for the vis data hierarchy
+    var root = {name:"W3C", children:[]};
     var totalIssues2 = 0;
-    this.data.groups.forEach(function(d) {
+    // Create a group object for every group in the original dataset
+    this.data.groups.forEach(function(_group) {
         var group = {};
-        group.name = d.name;
-        group.shortname = d.shortname;
-        group.url = d.url;
+        group.name = _group.name;
+        group.shortname = _group.shortname;
+        group.url = _group.url;
         group.children = [];
-        d.specs.forEach(function(dd) {
+        //specs are only keyed by url in each group, spec_lookup will be used to find them in the original specs dataset
+        _group.specs.forEach(function(_spec) {
             var spec = {};
-            spec.url = dd.url;
+            spec.url = _spec.url;
+            //every spec will have two children, a group of issues for the HTML spec and a group of tests
             spec.children = [{name:"HTML", children:[]},{name:"Tests", children:[]}];
-            if(that.displayData.spec_lookup[dd.url]) {
-                spec.name = that.displayData.spec_lookup[dd.url].name;
-                spec.children[0].children = that.displayData.spec_lookup[dd.url].issues;
+            if(that.displayData.spec_lookup[_spec.url]) {
+                spec.name = that.displayData.spec_lookup[_spec.url].name;
+                spec.children[0].children = that.displayData.spec_lookup[_spec.url].issues;
                 if(spec.children[0].children) {
                     totalIssues2 += spec.children[0].children.length
                 }
             } else {
-                console.log("Spec Not Found Error: " + dd.url);
+                console.log("Spec Not Found Error: " + _spec.url);
             }
-            if(that.displayData.test_lookup[dd.url]) {
-                spec.children[1].children = that.displayData.test_lookup[dd.url];
+            if(that.displayData.test_lookup[_spec.url]) {
+                spec.children[1].children = that.displayData.test_lookup[_spec.url];
             } else {
                //console.log("No Tests for: " + dd.url);
             }
             group.children.push(spec);
         });
-        that.displayData.root.children.push(group);
+        root.children.push(group);
     });
     console.log(totalIssues2);
 
-    var totalIssues = 0;
-    this.displayData.root.children.forEach(function(d) {
+    var totalIssues3 = 0;
+    root.children.forEach(function(d) {
         d.children.forEach(function(dd) {
             dd.children.forEach(function(ddd) {
                 if(ddd.children) {
                    // console.log(ddd.children);
-                    totalIssues+=ddd.children.length;
+                    totalIssues3+=ddd.children.length;
                 }
             });
         });
     });
-    console.log(totalIssues);
-    console.log(this.displayData.root);
+    console.log(totalIssues3);
+
+    this.displayData.root = root;
 };
 
 SpecVis.prototype.updateVis = function() {
     var that = this;
 
-    var click = function(d) {
-        console.log(d);
-    };
+    //TODO: UGLY UGLY HACK: creates a deep copy of the root object, because the partition layout creates all sorts of funky
+    //values on the live root object. Maybe there are issues with live object references?
+    var rootJSONString = JSON.stringify(this.displayData.root);
+    var root = JSON.parse(rootJSONString);
+
+
 
     // Setup for switching data: stash the old values for transition.
     var stash = function(d) {
@@ -157,45 +166,29 @@ SpecVis.prototype.updateVis = function() {
         d.dx0 = d.dx;
     };
 
-    // When switching data: interpolate the arcs in data space.
-    var arcTweenData = function(a, i) {
-        var oi = d3.interpolate({x: a.x0, dx: a.dx0}, a);
-        function tween(t) {
-            var b = oi(t);
-            a.x0 = b.x;
-            a.dx0 = b.dx;
-            return arc(b);
-        }
-        if (i == 0) {
-            // If we are on the first arc, adjust the x domain to match the root node
-            // at the current zoom level. (We only need to do this once.)
-            var xd = d3.interpolate(that.x.domain(), [node.x, node.x + node.dx]);
-            return function(t) {
-                that.x.domain(xd(t));
-                return tween(t);
-            };
-        } else {
-            return tween;
-        }
+
+    var click = function(d) {
+        console.log(d);
+        path.transition()
+            .duration(750)
+            .attrTween("d", that.arcTween(d));
     };
 
-    this.partition(this.displayData.root);
-
-    var path = this.svg.datum(this.displayData.root).selectAll("path")
-        .data(this.partition.nodes);
-    path
+    var path = this.svg.selectAll("path")
+        .data(this.partition(root))
         .enter().append("path")
-        .attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
         .attr("d", this.arc)
-        .style("stroke", "#fff")
-        .style("fill", function(d) {return that.color((d.children ? d : d.parent).name); })
-        .style("fill-rule", "evenodd")
-        .on("click", click)
-        .each(stash);
+        .style("fill", function(d) { return that.color((d.children ? d : d.parent).name); })
+        .on("click", click);
+
+
+    //var path = this.svg.selectAll("path")
+    //    .data(this.partition.nodes(this.displayData.root))
+    //    .enter().append("path")
+    //    .attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
+    //    .each(stash);
 
 //    path.attrTween("d", arcTweenData);
-//    path
-//        .exit().remove();
 
     //var text = this.svg.selectAll("text").data(this.partition.nodes);
     //var textEnter = text.enter().append("text")
@@ -226,3 +219,16 @@ SpecVis.prototype.onSelectionChange = function(selectionStart, selectionEnd) {
     this.wrangleData(function(d) {return new Date(d.created_at) >= selectionStart && new Date(d.created_at) <=selectionEnd});
     this.updateVis();
 };
+
+// Interpolate the scales!
+SpecVis.prototype.arcTween = function(d) {
+    var that = this;
+    var xd = d3.interpolate(this.x.domain(), [d.x, d.x + d.dx]),
+        yd = d3.interpolate(this.y.domain(), [d.y, 1]),
+        yr = d3.interpolate(this.y.range(), [d.y ? 20 : 0, that.radius]);
+    return function(d, i) {
+        return i
+            ? function(t) { return that.arc(d); }
+            : function(t) { that.x.domain(xd(t)); that.y.domain(yd(t)).range(yr(t)); return that.arc(d); };
+    };
+}
