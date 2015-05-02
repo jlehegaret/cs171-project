d110d92d7051d9be004a8a21393c9b3f7f5147f0
John Greeley <johnny.greeley@gmail.com>
2015-05-02 16:15:32 -0400

    wiring up status change filter events

--- dashboard/js/filterui.js
+++ dashboard/js/filterui.js
@@ -1,26 +1,21 @@
-FilterUI = function (_parentElement, _data, _eventHandler, _options) {
-    this.parentElement = _parentElement;
-    this.data = _data;
-    this.eventHandler = _eventHandler;
-    this.options = _options || {width: 800, height: 800};
-    this.displayData = {};
-    this.allIssues = [];
+FilterUI = function  (_filters, _eventHandler) {
 
-    // defines constants
-    this.margin = {top: 20, right: 20, bottom: 20, left: 50};
-    this.width = this.options.width - this.margin.left - this.margin.right;
-    this.height = this.options.height - this.margin.top - this.margin.bottom;
-
-    this.initVis();
-};
-
-FilterUI.prototype.initVis = function () {
     var that = this;
+    this.eventHandler = _eventHandler;
+    this.filters = _filters;
+
+    document.getElementById("state_open").addEventListener("click", function(d) {
+        that.filters.state = "open";
+        $(that.eventHandler).trigger("filterChanged", that.filters);
+    });
+
+    document.getElementById("state_all").addEventListener("click", function(d) {
+        that.filters.state = "all";
+        $(that.eventHandler).trigger("filterChanged", that.filters);
+    })
 
 
-    // constructs filter ui
-    this.ui = this.parentElement.append("form");
 
-    //TODO: Show all issues, open issues, closed issues
+
 };
 
