3f0bc0f1781c973b5d4c29026d3b6539364ccccf
John Greeley <johnny.greeley@gmail.com>
2015-05-02 16:40:09 -0400

    adding category event listeners

--- dashboard/js/filterui.js
+++ dashboard/js/filterui.js
@@ -8,12 +8,23 @@ FilterUI = function  (_filters, _eventHandler) {
         that.filters.state = "open";
         $(that.eventHandler).trigger("filterChanged", that.filters);
     });
-
     document.getElementById("state_all").addEventListener("click", function(d) {
         that.filters.state = "all";
         $(that.eventHandler).trigger("filterChanged", that.filters);
-    })
+    });
 
+    document.getElementById("cat_all").addEventListener("click", function(d) {
+        that.category = "all";
+        $(that.eventHandler).trigger("filterChanged", that.filters);
+    });
+    document.getElementById("cat_spec").addEventListener("click", function(d) {
+        that.category = "spec";
+        $(that.eventHandler).trigger("filterChanged", that.filters);
+    });
+    document.getElementById("cat_test").addEventListener("click", function(d) {
+        that.category = "test";
+        $(that.eventHandler).trigger("filterChanged", that.filters);
+    })
 
 
 
