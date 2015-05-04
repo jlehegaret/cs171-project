daa9bb49e3c5c2f35ed12a5940adaaa3dac30b5d
John Greeley <johnny.greeley@gmail.com>
2015-05-04 17:05:38 -0400

    adding who to filter tracking code

--- dashboard/js/filterui.js
+++ dashboard/js/filterui.js
@@ -10,6 +10,7 @@ FilterUI = function  (_parentElement, _filters, _eventHandler) {
 };
 
 FilterUI.prototype.addEventHandlers = function() {
+    var that = this;
     document.getElementById("state_open").addEventListener("click", function(d) {
         that.filters.state = "open";
         $(that.eventHandler).trigger("filterChanged", that.filters);
@@ -56,13 +57,23 @@ FilterUI.prototype.initUI = function() {
 
     this.timelineLabelStart = this.parentElement.select("#timeframeLabelStart");
     this.timelineLabelEnd = this.parentElement.select("#timeframeLabelEnd");
+    this.whoLabel = this.parentElement.select("#whoLabel");
 
     this.timelineLabelStart.text(this.dateFormatter(new Date(this.filters.start_date)));
     this.timelineLabelEnd.text(this.dateFormatter(new Date(this.filters.end_date)));
+    this.whoLabel.text(this.getAuthorText(this.filters.who));
 };
 
 FilterUI.prototype.onAuthorChange = function(author) {
+    this.whoLabel.text(this.getAuthorText(this.filters.who));
+};
 
+FilterUI.prototype.getAuthorText = function(author) {
+    if(!author) {
+        return "All"
+    } else {
+        return author;
+    }
 };
 
 FilterUI.prototype.onTimelineChange = function(startDate, endDate) {
